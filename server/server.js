3b013dfcb7a062cfa6fc0eff1472fd75894c29ab
Cherie2 <cwalk280@yahoo.com>
2023-02-27 14:41:17 -0800

    Revised server

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ server.applyMiddleware({app});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// If we're in production, serve client/build as static assets
+// If in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
